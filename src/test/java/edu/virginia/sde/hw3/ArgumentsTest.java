4613d87997ad09618f1930dd7635346fa0498dfd
WillMcBurney <pmcburn1@gmail.com>
2024-09-23 14:19:55 -0400

    Added additional testing around displayorder

--- src/test/java/edu/virginia/sde/hw3/ArgumentsTest.java
+++ src/test/java/edu/virginia/sde/hw3/ArgumentsTest.java
@@ -82,6 +82,56 @@ public class ArgumentsTest {
         assertEquals("output.csv", csvOutputFile.getOutputFilename());
     }
 
+    @Test
+    public void argumentsTest_inputFile_repsSpecified_populationFormat_descendingDefault() {
+        String resourceFilename = getResource(TEST_CSV_LOCATION);
+        String[] args = {resourceFilename, "100", "--population"};
+        Arguments arguments = new Arguments(args);
+
+        assertEquals(100, arguments.getTargetRepresentatives());
+
+        StateSupplier stateSupplier = arguments.getStateSupplier();
+        assertInstanceOf(CSVStateFile.class, stateSupplier);
+        String filename = ((CSVStateFile) stateSupplier).getCsvFile().getPath();
+        assertTrue(filename.endsWith(TEST_CSV_LOCATION),
+                "Bad filename: " + filename);
+
+        ApportionmentMethod method = arguments.getApportionmentMethod();
+        assertInstanceOf(JeffersonMethod.class, method);
+
+        RepresentationFormat format = arguments.getRepresentationFormat();
+        assertInstanceOf(PopulationFormat.class, format);
+        PopulationFormat populationFormat = (PopulationFormat) format;
+        assertEquals(DisplayOrder.DESCENDING, populationFormat.getDisplayOrder());
+
+        assertTrue(arguments.getOutputFile().isEmpty());
+    }
+
+    @Test
+    public void argumentsTest_inputFile_defaultReps_populationFormat_ascendingSpecified() {
+        String resourceFilename = getResource(TEST_CSV_LOCATION);
+        String[] args = {resourceFilename, "--population", "--ascending"};
+        Arguments arguments = new Arguments(args);
+
+        assertEquals(Arguments.DEFAULT_REPRESENTATIVES, arguments.getTargetRepresentatives());
+
+        StateSupplier stateSupplier = arguments.getStateSupplier();
+        assertInstanceOf(CSVStateFile.class, stateSupplier);
+        String filename = ((CSVStateFile) stateSupplier).getCsvFile().getPath();
+        assertTrue(filename.endsWith(TEST_CSV_LOCATION),
+                "Bad filename: " + filename);
+
+        ApportionmentMethod method = arguments.getApportionmentMethod();
+        assertInstanceOf(JeffersonMethod.class, method);
+
+        RepresentationFormat format = arguments.getRepresentationFormat();
+        assertInstanceOf(PopulationFormat.class, format);
+        PopulationFormat populationFormat = (PopulationFormat) format;
+        assertEquals(DisplayOrder.ASCENDING, populationFormat.getDisplayOrder());
+
+        assertTrue(arguments.getOutputFile().isEmpty());
+    }
+
     @SuppressWarnings("SameParameterValue")
     private String getResource(String resourceName) {
         ClassLoader classLoader = getClass().getClassLoader();
